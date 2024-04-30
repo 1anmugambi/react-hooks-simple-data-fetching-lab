aa82a899deee6a2e0b2336ea9d2ab2a8a5b2c864
Ian Mugambi <ian.mugambi1@student.moringaschool.com>
2024-04-30 12:29:55 +0300

    Complete Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    const fetchDogImage = async () => {
+      try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        if (!response.ok) {
+          throw new Error("Failed to fetch dog image");
+        }
+        const data = await response.json();
+        setDogImage(data.message);
+      } catch (error) {
+        console.error("Error while fetching dog image:", error.message);
+      }
+    };
+
+    fetchDogImage();
+  }, []);
+
+  return (
+    <>
+      {dogImage ? (
+        <img src={dogImage} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
+  );
+}
+
+export default App;
